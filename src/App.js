ae029464e6a0c7cfe2b7fc5effd4e26910030a5d
tarun2506 <tarunsharma8920@gmail.com>
2021-07-18 21:50:05 +0530

    basic input functionality added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [todos, setTodos] = useState([
+    "Make Breakfast",
+    "Play Guitar",
+    "Do one react project",
+    "Coding for an hour",
+  ]);
+  const [input, setInput] = useState("");
+  const addTodo = (e) => {
+    e.preventDefault();
+    setTodos([...todos, input]);
+    setInput("");
+  };
   return (
     <div className="App">
       <h1>Hello World! 🚀 </h1>
+      <form>
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button type="submit" onClick={addTodo}>
+          Add Todo
+        </button>
+      </form>
+      <ul>
+        {todos.map((todo) => (
+          <li>{todo}</li>
+        ))}
+      </ul>
     </div>
   );
 }
